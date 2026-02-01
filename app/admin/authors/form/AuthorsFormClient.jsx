4db6a9f5736eba913eade466bcce9117f9520c94
ZubairDevCoder <zubairdevcoder@gmail.com>
2026-02-01 15:22:31 +0500

    working ...

--- app/admin/authors/form/AuthorsFormClient.jsx
+++
@@ -1,6 +0,0 @@
-import AuthorsFormClient from "./AuthorsFormClient";
-
-// ✅ Purely server side page just renders client component
-export default function Page() {
-  return <AuthorsFormClient />;
-}
